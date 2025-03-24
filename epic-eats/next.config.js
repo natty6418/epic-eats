da4a55adc4fe67094d5033fd32546f2b3e73ef31
natty6418 <natnaelmetekie@gmail.com>
2025-03-24 22:28:11 +0400

    fixed mogoose issue

--- epic-eats/next.config.js
+++ epic-eats/next.config.js
@@ -3,6 +3,18 @@ const nextConfig = {
     experimental: {
         forceSwcTransforms: true,
       },
+      webpack: (config, { isServer }) => {
+        if (!isServer) {
+          config.externals.push('@mongodb-js/zstd');
+        }
+    
+        config.module.rules.push({
+          test: /\.node$/,
+          use: 'ignore-loader'
+        });
+    
+        return config;
+      }
 }
 
 module.exports = nextConfig
